24f10c6705ff60dbacfe81930c056769f8689183
DarianMD <darianmanasturean@gmail.com>
2023-09-27 19:46:50 +0200

    test2

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <raylib.h>
+
+
+int main(){
+    
+    InitWindow(450,400, "Hello");
+
+
+    while(!WindowShouldClose()){
+
+        
+    }
+    CloseWindow();
+    return 0;
+}
